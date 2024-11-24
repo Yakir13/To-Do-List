039dd3e5ac53e3821ceedd741c58fab8a0022a22
Yakir13 <yakir.helfer100@gmail.com>
2024-11-25 00:45:17 +0200

    due date

--- To-Do List.py
+++ To-Do List.py
@@ -29,11 +29,42 @@ def remove(tasks):
         print(Fore.RED + "Please enter a valid number!")
 
 
+def due_date(tasks):
+    try:
+        task_number = int(input("Enter the number of the task to set a due date: "))
+        if 1 <= task_number <= len(tasks):
+            due_date = input("Enter the due date (YYYY-MM-DD): ").strip()
+            try:
+                datetime.datetime.strptime(due_date, "%Y-%m-%d")
+                current_task, timestamp = tasks[task_number - 1]
+                tasks[task_number - 1] = (current_task, timestamp, due_date)
+                print(
+                    Fore.GREEN
+                    + f"Due date set to '{due_date}' for task '{current_task}'."
+                )
+            except ValueError:
+                print(Fore.RED + "Invalid date format! Please use YYYY-MM-DD.")
+        else:
+            print(Fore.RED + "Invalid task number.")
+    except ValueError:
+        print(Fore.RED + "Please enter a valid number.")
+
+
+def view(tasks):
+    if tasks:
+        print("\nYour Tasks: ")
+        for i, (task) in enumerate(tasks, start=1):
+            print(Fore.GREEN + f"{i}. {task}")
+    else:
+        print(Fore.RED + "\nNo tasks yet! Add a task to get started.")
+
+
 def edit(tasks):
+    view(tasks)
     try:
         task_number = int(input("\nEnter the number of the task you want to edit: "))
         if 1 <= task_number <= len(tasks):
-            current_task, _ = tasks[task_number - 1]
+            current_task = tasks[task_number - 1]
             new_task = input(f"Edit task '{current_task}': ").strip()
             if new_task:
                 timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -81,26 +112,32 @@ def menu():
     print(Fore.YELLOW + "\nTo-Do List Menu")
     print(Fore.BLUE + "1. Add Task")
     print(Fore.BLUE + "2. Remove Task")
-    print(Fore.BLUE + "3. Edit Task")
-    print(Fore.BLUE + "4. Search Task")
-    print(Fore.BLUE + "5. Save Tasks")
-    print(Fore.BLUE + "6. Exit")
+    print(Fore.BLUE + "3. View Task")
+    print(Fore.BLUE + "4. Due Date")
+    print(Fore.BLUE + "5. Edit Task")
+    print(Fore.BLUE + "6. Search Task")
+    print(Fore.BLUE + "7. Save Tasks")
+    print(Fore.BLUE + "8. Exit")
 
 
 while True:
     menu()
-    choice = input("\nChoose an option (1-6): ")
+    choice = input("\nChoose an option (1-7): ")
     if choice == "1":
         add(tasks)
     elif choice == "2":
         remove(tasks)
     elif choice == "3":
-        edit(tasks)
+        view(tasks)
     elif choice == "4":
-        search(tasks)
+        due_date(tasks)
     elif choice == "5":
-        save(tasks)
+        edit(tasks)
     elif choice == "6":
+        search(tasks)
+    elif choice == "7":
+        save(tasks)
+    elif choice == "8":
         print(Fore.GREEN + "Goodbye!")
         break
     else:
