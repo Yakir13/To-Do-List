a577018f66023e376ba9ebb64a0820afc464a6bc
Yakir13 <yakir.helfer100@gmail.com>
2025-04-15 00:48:39 +0300

    rich

--- app.py
+++ app.py
@@ -1,46 +1,59 @@
-from colorama import Fore
+# from colorama import Fore, Style, Back
+from rich.prompt import Prompt
+from rich import print
 import datetime
-import os
+import time
 import json
+import os
 
 todo_list = []
 
 def menu():
-    print(Fore.BLUE + "\nTo-Do List Menu\n" + "-" * 20)
-    print("1. Add Task")
-    print("2. View Task")
-    print("3. Remove Task")
+    print("[bold blue]\nTo-Do List Menu\n" + "-" * 20)
+    print("[bold blue]1. Add Task")
+    print("[bold blue]2. View Task")
+    print("[bold blue]3. Remove Task")
+    print("[bold blue]4. Edit")
 
     
 def add():
-    task = input(Fore.CYAN + "\nEnter a task: ")
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    task = Prompt.ask("[bold yellow]\nEnter a task")
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d || %H:%M:%S")
     todo_list.append((task, timestamp))
-    print(f"Task '{task}' added! ({timestamp}).")
+    time.sleep(2)
+    print(f"[bold green]\nTask '{task}' added! ({timestamp}).")
     
 def view():
     if not todo_list:
-        print(Fore.CYAN + "\nYour to-do list in empty.")
+        print("[bold red]\nYour to-do list is empty.")
     else:
-        print(Fore.CYAN + "\nYour Tasks:")
+        print("[bold yellow]\nYour Tasks:")
         for idx, (task, timestamp)  in enumerate(todo_list, start=1):
-            print(f"{idx}. {task} (added on {timestamp})")
+            print(f"[bold green]{idx}. {task} (added on: {timestamp})")
             
 def remove():
     view()
     try:
-        task_num = int(input(Fore.CYAN + "\nEnter the number of the taks to remove: "))
-        if 1 <= task_num <= len(todo_list):
-            removed = todo_list.pop(task_num - 1)
-            print(Fore.RED + f"Task '{removed[0]}' removed.")
-        else:
-            print("Invalid task number.")
-    except ValueError:
-        print("Please enter a valid number.")
+        task_num = int(Prompt.ask("[bold yellow]\nEnter task number to remove"))
+        removed = todo_list.pop(task_num - 1)
+        print(f"[bold red]\nTask '{removed[0]}' removed.")
+    except (IndexError, ValueError):
+        print("[bold red]\nInvalid input!")
+
+def edit():
+    view()
+    try:
+        task_num = int(Prompt.ask("[bold yellow]\nEnter task number to edit"))
+        new_text = Prompt.ask("[bold yellow]\nEnter new task description")
+        task, status = todo_list[task_num - 1]
+        todo_list[task_num - 1] = (new_text, status)
+        print(f"[bold green]\nTask '{task}' updated to '{new_text}'.")
+    except:
+        print("[bold red]\nInvalid input.")
     
 while True:
     menu()
-    choice = input("Choose an option: ")
+    choice = Prompt.ask("[bold cyan]\nChoose an option")
     
     if choice == "1":
         add()
@@ -48,3 +61,5 @@ while True:
         view() 
     if choice == "3":
         remove()
+    if choice == "4":
+        edit()
